eb7f9f73fdf68108b4f6bf14fc157c54660c66d2
Marcos353 <141325101+Marcos353@users.noreply.github.com>
2023-10-26 13:40:33 -0300

    Add files via upload

---
+++ Cadastro de Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Criando_novo_projeto_MAB
+{
+    public partial class Cadastro_de_Cliente : Form
+    {
+        public Cadastro_de_Cliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
